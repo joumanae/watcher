549482b29443f1417e8e96174297f6ca2bffec45
Joumana El Alaoui <joumana.codes@gmail.com>
2024-02-05 12:57:49 -0600

    WIP:Functions to some degree

--- go.mod
+++ go.mod
@@ -5,3 +5,11 @@ go 1.21.3
 // require gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 
 // require gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+
+require github.com/fatih/color v1.16.0
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+)
--- watcher.go
+++ watcher.go
@@ -2,27 +2,62 @@ package watcher
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"os/signal"
 	"strings"
+
+	"github.com/fatih/color"
 )
 
-type Check struct {
+type Checker struct {
 	url     string
 	keyword string
+	Output  io.Writer
 }
 
-func NewCheck(url string, keyword string) *Check {
-	return &Check{
+func NewChecker(url string, keyword string) *Checker {
+	return &Checker{
 		url:     url,
 		keyword: keyword,
+		Output:  os.Stdout,
 	}
 }
 
+// Fetch the url and verify if the keyword is on the page fetched.
+// https://innercitytennis.clubautomation.com/calendar/programs 6U Red Rockets https://www.americankaratestudio.com/stlouispark KIDS GREEN-PURPLE
+// When I fetch this, it does not look at images
+func Fetch(url string, keyword string) (matched bool, err error) {
+
+	resp, err := http.Get(string(url))
+	if err != nil {
+		return false, fmt.Errorf("the url was not fetched, %v", err)
+	}
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return false, fmt.Errorf("there was an issue reading the response %v", err)
+	}
+	sr := string(b)
+	return strings.Contains(sr, keyword), nil
+}
+
+func (c *Checker) PrintInformation(ctx context.Context) {
+	matched, err := Fetch(c.url, c.keyword)
+	if err != nil {
+		fmt.Fprintf(c.Output, "There was an issue fetching the url %s", err)
+	}
+	if !matched {
+		fmt.Fprintf(c.Output, "[%s] No additional information about %s is available on the page %s \n", color.RedString("CHECKED-NO INFO"), c.keyword, c.url)
+
+	}
+	fmt.Fprintf(c.Output, "[%s], There is information about %s. on the page %s\n", color.GreenString("CHECKED"), c.keyword, c.url)
+}
+
 // Start a list of urls.
-func (c *Check) StartList() (string, error) {
+func (c *Checker) StartList() (string, error) {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	filePath := scanner.Text()
@@ -41,7 +76,7 @@ func (c *Check) StartList() (string, error) {
 }
 
 // Read the file and create a map with urls and keywords.
-func (c *Check) ReadFileSaveInput(filePath string) (map[string]string, error) {
+func (c *Checker) ReadFileSaveInput(filePath string) (map[string]string, error) {
 	inputData := make(map[string]string)
 	file, err := os.Open(filePath)
 	if err != nil {
@@ -50,38 +85,29 @@ func (c *Check) ReadFileSaveInput(filePath string) (map[string]string, error) {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
+	for i := 1; scanner.Scan(); i++ {
 		input := scanner.Text()
-		parts := strings.SplitN(input, " ", 2)
-		c.url = parts[0]
-		c.keyword = parts[1]
-		inputData[c.url] = c.keyword
+		if i%2 == 0 {
+			c.keyword = input
+		} else {
+			c.url = input
+		}
+
+		inputData[c.keyword] = c.url
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
 	}
 	return inputData, nil
 }
 
-// Fetch the url and verify if the keyword is on the page fetched.
-// https://innercitytennis.clubautomation.com/calendar/programs, 6U Red Rockets, https://www.americankaratestudio.com/stlouispark KIDS GREEN-PURPLE
-// When I fetch this, it does not look at images
-func Fetch(url string, keyword string) (matched bool, err error) {
-
-	resp, err := http.Get(string(url))
-	if err != nil {
-		return false, fmt.Errorf("the url was not fetched, %v", err)
-	}
-	b, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return false, fmt.Errorf("there was an issue reading the response %v", err)
-	}
-	sr := string(b)
-	return strings.Contains(sr, keyword), nil
-}
-
 // Run the program
 func Main() int {
+	var c Checker
 
-	var c Check
-	NewCheck(c.keyword, c.url)
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
 	filepath, err := c.StartList()
 	if err != nil {
 		return 1
@@ -91,10 +117,25 @@ func Main() int {
 	if err2 != nil {
 		return 1
 	}
-	fmt.Println("Here is the map", MapedFile)
 
-	for c.url, c.keyword = range MapedFile {
-		fmt.Println(Fetch(c.url, c.keyword))
+	resultCh := make(chan int)
+	for url, keyword := range MapedFile {
+		go func(u, k string) {
+			defer func() {
+				resultCh <- 0 // Indicate completion
+			}()
+			checker := NewChecker(u, k)
+			fmt.Println("inside the loop")
+			fmt.Println("here is the url", u)
+			fmt.Println("here is the keyword", k)
+			checker.PrintInformation(ctx)
+		}(url, keyword)
 	}
+
+	// Wait for all goroutines to complete
+	for range MapedFile {
+		<-resultCh
+	}
+
 	return 0
 }
